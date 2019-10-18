dcec5fd0921773dbadf976393cb5fce3e3dca508
RomanSolodukhin <mauvaistonn@gmail.com>
2019-10-18 10:30:00 +0000

    Added efbalens_ddt

---
+++ efbalens_ddt.js
@@ -0,0 +1,24 @@
+var testData = new Object();
+testData = {
+	users: {
+		username: ["mauvaistonn@gmail.com","tester.jopsky@test.tst","tester4.tk@test.tst","tester30.tk@test.tst","tester55.tk@test.tst", "tester5.tk@test.tst"],
+		pass: ["kjgfnjxrf777","kjgfnjxrf","123456","123456","123456","kjgfnjxrf"]
+	},
+	browsers: ["chrome", "firefox"],
+	resolutions: [
+	{x: 1920, y: 1080},
+	{x: 1366, y: 768},
+	{x: 1024, y: 768},
+	{x: 360, y: 640},
+	{x: 1536, y: 864},
+	{x: 1440, y: 900},
+	{x: 1600, y: 900}
+	],
+	server_data_id: [4443,7401],
+	paymentset: [150, 300, 600, 900, 1500, 3000],
+	baseUrl: "https://crea:Deifiey3@dev.creagames.ru/",
+	times: 10
+};
+exports.testData = testData;
+exports.times = testData.times;
+exports.baseUrl = testData.baseUrl;
