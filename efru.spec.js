bcf6011bc1da1f626d2f71f945629b18f8d1a714
RomanSolodukhin <mauvaistonn@gmail.com>
2019-11-19 13:04:45 +0300

    android docker testing

--- efru.spec.js
+++ efru.spec.js
@@ -12,9 +12,9 @@ describe('Eternal Fury RU', function() {
   let testName = String(this.title)
   before(async function() {
     var capabilities = {
-      browserName: 'chrome',
-      version: '78.0',
-      enableVNC: true,
+      browserName: 'android',
+      version: '8.1',
+      enableVNC: false,
       enableLog: true,
       name: testName,
       enableVideo: true
