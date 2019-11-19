821786e66c6b76de45cff1f61ee3d4af197adc69
RomanSolodukhin <mauvaistonn@gmail.com>
2019-11-19 11:31:23 +0300

    r

--- efru.spec.js
+++ efru.spec.js
@@ -9,7 +9,7 @@ describe('Eternal Fury RU', function() {
   let driver
   let site = "https://www.creagames.com/"
   let MAX_SERVERS = 1
-  console.log("Название теста: ""+String(this.title))
+  console.log("Название теста: "+String(this.title))
   before(async function() {
     var capabilities = {
       browserName: 'chrome',
