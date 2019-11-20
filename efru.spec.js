ab213c57d1022eeed49ee08b87de8ea1811c1346
roma <mauvaistonn@gmail.com>
2019-11-21 00:02:43 +0300

    fix

--- efru.spec.js
+++ efru.spec.js
@@ -46,10 +46,8 @@ describe('Авторизация', function(done) {
   afterEach(async function() {
     //if(this.currentTest.err) throw new Error(this.currentTest.err)
     let name = String(this.currentTest.title)
-allure.createStep("saveScreenshot", async function() {
     var res = await driver.takeScreenshot();
     allure.createAttachment(name, 'жопа')
-})
   })
   it('Загрузить страницу', async function() {
     await driver.get(site)
