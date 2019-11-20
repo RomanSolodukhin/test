5c91462e305c16cf5a7ed924fc020822329d3d8a
RomanSolodukhin <mauvaistonn@gmail.com>
2019-11-20 14:57:14 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -1,6 +1,7 @@
 const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
-
+console.log(allure)
+console.log(Allure)
 describe('Eternal Fury RU', function() {
   this.timeout(10000)
   this.slow(1000)
