21fc13c8c77ac8aadf1f2be4ea26e8f0e17742d9
roma <mauvaistonn@gmail.com>
2019-11-21 00:31:48 +0300

    fix

--- efru.spec.js
+++ efru.spec.js
@@ -79,8 +79,8 @@ describe('Авторизация', function(done) {
   })
   it('Авторизоваться', async function() {
     await driver.findElement(By.id("loginform-password")).sendKeys(Key.ENTER)
-    await driver.wait(until.elementLocated(By.css(".g-header_profile_data_item")),30000)
-    await driver.wait(until.elementIsVisible(driver.findElement(By.css(".g-header_profile_data_item"))))
+    await driver.wait(until.elementLocated(By.css(".g-header_profile_data_name")),30000)
+    await driver.wait(until.elementIsVisible(driver.findElement(By.css(".g-header_profile_data_name"))))
   })
   it('Выбрать игру', async function() {
     await driver.actions().move({origin: driver.findElement(By.css(".has_submenu:nth-child(1)"))}).perform()
