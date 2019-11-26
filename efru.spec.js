e9e83c05aa5edb24a546efe483aa9347fb3524d9
RomanSolodukhin <mauvaistonn@gmail.com>
2019-11-26 14:21:28 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -90,7 +90,7 @@ describe('Авторизация', function(done) {
   })
   it('Ввести учетные данные', async function() {
     await driver.findElement(By.id("loginform-username")).sendKeys("r.solodukhin@creagames.com")
-    await driver.findElement(By.id("loginform-password")).sendKeys("123456qQ_WRONG")
+    await driver.findElement(By.id("loginform-password")).sendKeys("123456qQ")
   })
   it('Авторизоваться', async function() {
       await driver.findElement(By.id("loginform-password")).sendKeys(Key.ENTER)
