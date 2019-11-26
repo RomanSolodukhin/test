6ce165c18a79d4e19c36c8cd83808ca1636836f8
RomanSolodukhin <mauvaistonn@gmail.com>
2019-11-26 16:56:27 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -61,7 +61,7 @@ describe('Авторизация', function(done) {
       allure.createAttachment(name, new Buffer(res, 'base64'))
       allure.createAttachment('Отчёт', String(this.currentTest.err))
       allure.severity('blocker')
-      assert.fail('Тест остановлен. '+this.currentTest.err)
+      //assert.fail('Тест остановлен. '+this.currentTest.err)
     }
   })
   it('Загрузить страницу', async function() {
@@ -112,6 +112,7 @@ describe('Авторизация', function(done) {
                   message: 'Не получилось найти элемент с ошибкой формы. '+err.message
                 })
       }
+      assert.fail(err)
     }
   })
   it('Выбрать игру', async function() {
