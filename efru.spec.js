24740abd2eb0d5090bebcfd76596e953bef85575
RomanSolodukhin <mauvaistonn@gmail.com>
2019-11-26 14:34:13 +0300

    НЕПРАВИЛЬНЫЕ УЧЕТНЫЕ ДАННЫЕ

--- efru.spec.js
+++ efru.spec.js
@@ -89,7 +89,7 @@ describe('Авторизация', function(done) {
   })
   it('Ввести учетные данные', async function() {
     await driver.findElement(By.id("loginform-username")).sendKeys("r.solodukhin@creagames.com")
-    await driver.findElement(By.id("loginform-password")).sendKeys("123456qQ")
+    await driver.findElement(By.id("loginform-password")).sendKeys("123456qQ_WRONG")
   })
   it('Авторизоваться', async function() {
     try {
