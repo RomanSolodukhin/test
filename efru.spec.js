26de937c49e76bbbe8cee3b48b306973880facb1
RomanSolodukhin <mauvaistonn@gmail.com>
2019-11-29 13:27:02 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -87,8 +87,8 @@ describe('Eternal Fury RU', function() {
       reportName: process.env.GIT_BRANCH+'/'+process.env.GIT_COMMIT+'/'+process.env.GIT_COMMITTER_NAME,
       reportUrl: process.env.GIT_URL
   };
-
-  allure.AddExecutor('jenkins')
+  await allure.addLabel('testLabel','testLabelValue')
+  await allure.AddExecutor(jenkinsEnv)
   //await jObject.Executor('allure-results', jenkinsEnv)
   /*
     var fs = require('fs-extra'),
