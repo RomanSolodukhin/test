635a1458ffb200457987ad24210ba3460560d252
RomanSolodukhin <mauvaistonn@gmail.com>
2019-11-29 11:00:07 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -82,7 +82,7 @@ describe('Eternal Fury RU', function() {
     };
     var fs = require('fs-extra'),
         path = require('path');
-        fs.outputJsonSync('./executor.json', jenkinsEnv);
+        fs.outputFileSync('./executor.json', JSON.stringify(jenkinsEnv));
     console.log('Альтернативный скрипт')
   })
 
