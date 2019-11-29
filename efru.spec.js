d70673cb34a0c99e77145e0815f7fbdf1cae1425
RomanSolodukhin <mauvaistonn@gmail.com>
2019-11-29 11:53:12 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -78,6 +78,7 @@ describe('Eternal Fury RU', function() {
       buildOrder: process.env.BUILD_NUMBER,
       buildName: process.env.JOB_NAME+' '+process.env.BUILD_DISPLAY_NAME,
       buildUrl: process.env.BUILD_URL,
+      reportUrl: process.env.GIT_URL,
       reportName: process.env.GIT_BRANCH+'/'+process.env.GIT_COMMIT+'/'+process.env.GIT_COMMITTER_NAME
     };
     var fs = require('fs-extra'),
