ce6eccb622e074d232b6b90a897025819766da93
RomanSolodukhin <mauvaistonn@gmail.com>
2019-11-29 11:35:48 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -76,7 +76,7 @@ describe('Eternal Fury RU', function() {
       type: "jenkins",
       url: process.env.JENKINS_URL,
       buildOrder: process.env.BUILD_NUMBER,
-      buildName: process.env.BUILD_NAME+' '+process.env.BUILD_DISPLAY_NAME,
+      buildName: process.env.JOB_NAME+' '+process.env.BUILD_DISPLAY_NAME,
       buildUrl: process.env.BUILD_URL,
       reportName: process.env.GIT_BRANCH/process.env.GIT_COMMIT/process.env.GIT_COMMITTER_NAME,
       git_branch: process.env.GIT_BRANCH,
