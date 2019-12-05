62cc453e40401c92e2889c261d1b9e410bcf48be
RomanSolodukhin <mauvaistonn@gmail.com>
2019-12-05 19:02:26 +0300

    f

--- creapay.js
+++ creapay.js
@@ -98,7 +98,7 @@ describe('Авторизация', function(done) {
   })
   it('Загрузить страницу', async function() {
     await driver.get(site)
-		await driver.wait(until.elementLocated(By.linkText("Вход")), 40000)
+		await driver.wait(until.elementLocated(By.linkText("Вход")), 30000)
   })
   it('Открыть форму авторизации', async function() {
     await driver.findElement(By.linkText("Вход")).click()
