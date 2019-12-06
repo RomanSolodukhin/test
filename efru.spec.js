9a4c133b8355e7fdd1c76cca9664a562894080ac
roma <mauvaistonn@gmail.com>
2019-12-06 19:38:24 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -94,7 +94,7 @@ describe('Авторизация', function(done) {
       allure.severity('blocker')
       removeVideo = false
       scriptBlocker = true
-      if(this.currentTest.title == 'Авторизоваться') assert.notEqual(await driver.findElement(By.id("loginform-password")).getAttribute('class'), 'b-input error', await driver.findElement(By.id("loginform-password")).getAttribute('title'))
+      //if(this.currentTest.title == 'Авторизоваться') assert.notEqual(await driver.findElement(By.id("loginform-password")).getAttribute('class'), 'b-input error', await driver.findElement(By.id("loginform-password")).getAttribute('title'))
 
       //assert.fail('Тест остановлен. '+this.currentTest.err)
     }
@@ -136,7 +136,7 @@ describe('Авторизация', function(done) {
       await driver.wait(until.elementIsVisible(driver.findElement(By.css(".g-header_profile_data_name"))))
     }
     catch(err) {
-      assert.notEqual(await driver.findElement(By.id("loginform-password")).getAttribute('class'), 'b-input error', await driver.findElement(By.id("loginform-password")).getAttribute('title'))
+      assert.notEqual(await driver.findElement(By.id("loginform-password")).getAttribute('class'), 'b-input error', 'Auth Error: '+await driver.findElement(By.id("loginform-password")).getAttribute('title'))
     }
   })
   it('Выбрать игру', async function() {
