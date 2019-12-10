54d3705431200d743f8d55a12032e11fd562863e
RomanSolodukhin <mauvaistonn@gmail.com>
2019-12-10 17:57:25 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -134,6 +134,7 @@ describe('Авторизация', function(done) {
   })
   it('Авторизация успешна', async function() {
     this.test.severity = 'blocker'
+    allure.severity('blocker')
     assert.notEqual(await driver.findElement(By.id("loginform-password")).getAttribute('class'), 'b-input error', 'Error: '+await driver.findElement(By.id("loginform-password")).getAttribute('title'))
     await driver.wait(until.elementLocated(By.css(".g-header_profile_data_name")),30000)
     await driver.wait(until.elementIsVisible(driver.findElement(By.css(".g-header_profile_data_name"))))
