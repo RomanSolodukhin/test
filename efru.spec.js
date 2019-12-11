3db019185ec3042337729df31ea6b60b8c09d9d0
RomanSolodukhin <mauvaistonn@gmail.com>
2019-12-11 15:30:56 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -134,8 +134,9 @@ describe('Авторизация', function(done) {
   it('Отправить форму', async function() {
     await driver.findElement(By.id("loginform-password")).sendKeys(Key.ENTER)
     let formSubmission = new Promise(function(resolve, reject) {
-      driver.wait(until.elementIsNotVisible(driver.findElement(By.id("loginform-password"))))
-      resolve(true)
+      (async() => {
+        await driver.wait(until.elementIsNotVisible(driver.findElement(By.id("loginform-password")))) => resolve(true)
+      })
     })
     await formSubmission.then(function(value) {
       assert.equal(value, true, 'Форма авторизации не закрыта автоматически')
