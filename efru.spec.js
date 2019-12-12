eb6c58c3bfeb6480822f6b8f210cfca9f759442b
RomanSolodukhin <mauvaistonn@gmail.com>
2019-12-12 19:01:48 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -84,7 +84,7 @@ describe('Авторизация', function(done) {
 
   afterEach(async function() {
     for(let i = 0; i < testSteps.length; i++) {
-      allure.createStep(testSteps[i], function() { 
+      allure.createStep(testSteps[i], async function() { 
         var res = await driver.takeScreenshot();
         allure.createAttachment('image', new Buffer(res, 'base64'))
       })
