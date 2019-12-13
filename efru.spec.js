0f76d1a1c8c306943eda141213cd8762fcda9e49
RomanSolodukhin <mauvaistonn@gmail.com>
2019-12-13 22:55:06 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -127,7 +127,7 @@ describe('Авторизация', function(done) {
     this.test.severity = 'blocker'
     function step(description, fnBody) {
       let newStep = new Promise(async function (resolve, reject) {
-        let fnResult = await allure.createStep(description, fnBody())
+        let fnResult = await allure.createStep(description, fnBody)
           if(fnResult) {
             console.log(fnResult)
             resolve(fnResult)
