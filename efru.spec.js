0fd92d46afd3fafd8e3032d183f331c15eea4d1f
RomanSolodukhin <mauvaistonn@gmail.com>
2019-12-13 19:32:41 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -126,32 +126,30 @@ describe('Авторизация', function(done) {
   it('Авторизация', function() {
     this.test.severity = 'blocker'
     function step(description, fnBody) {
-      var newStep = allure.createStep(description, fnBody)
+      var newStep = allure.createStep(description, async() => {
+        fnBody
+        assert.ok(true)
+      })
       return newStep()
     }
-      step('Открыть форму авторизации', async function() {
+      step('Открыть форму авторизации',
         await driver.findElement(By.linkText("Вход")).click()
         await driver.wait(until.elementLocated(By.id("loginModal")))
         await driver.wait(until.elementIsVisible(driver.findElement(By.id("loginModal"))))
-        assert.ok(true)
-      });
+      );
       step('Ввести логин', async function() {
         await driver.wait(until.elementLocated(By.id("loginform-username")))
         await driver.findElement(By.id("loginform-username")).sendKeys("r.solodukhin@creagames.com")
-        assert.ok(true)
-      });
+      })
       step('Ввести пароль', async function() {
         await driver.findElement(By.id("loginform-password")).sendKeys("123456qQ")
-        assert.ok(true)
       });
       step('Отправить форму', async function() {
         await driver.findElement(By.id("loginform-password")).sendKeys(Key.ENTER)
-        assert.ok(true)
       });
       step('Авторизация успешна', async function() {
         await driver.wait(until.elementLocated(By.css(".g-header_profile_data_name")),30000)
         await driver.wait(until.elementIsVisible(driver.findElement(By.css(".g-header_profile_data_name"))))
-        assert.ok(true)
       });
   })
 
