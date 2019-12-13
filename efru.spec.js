47ed7c72bfff292e7f625bb5ebd5a6726c6112a3
RomanSolodukhin <mauvaistonn@gmail.com>
2019-12-13 21:20:55 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -146,12 +146,12 @@ describe('Авторизация', function(done) {
       return value
     })
   }
-      step('Открыть форму авторизации', async() {
+      step('Открыть форму авторизации', async() => {
         await driver.findElement(By.linkText("Вход")).click()
         await driver.wait(until.elementLocated(By.id("loginModal")))
         await driver.wait(until.elementIsVisible(driver.findElement(By.id("loginModal"))))
       });
-      step('Ввести логин', async() {
+      step('Ввести логин', async() => {
         await driver.wait(until.elementLocated(By.id("loginform-username")))
         await driver.findElement(By.id("loginform-username")).sendKeys("r.solodukhin@creagames.com")
       });
