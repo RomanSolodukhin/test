213bbb1c62d73c0e4cbd5b4b46009460a2f4af32
RomanSolodukhin <mauvaistonn@gmail.com>
2019-12-13 17:58:20 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -129,26 +129,21 @@ describe('Авторизация', function(done) {
         await driver.findElement(By.linkText("Вход")).click()
         await driver.wait(until.elementLocated(By.id("loginModal")))
         await driver.wait(until.elementIsVisible(driver.findElement(By.id("loginModal"))))
-        assert.ok(true)
-      })
+      }, assert.ok(true))
       allure.createStep('Ввести логин', async() => {
         await driver.wait(until.elementLocated(By.id("loginform-username")))
         await driver.findElement(By.id("loginform-username")).sendKeys("r.solodukhin@creagames.com")
-        assert.ok(true)
-      })
+      }, assert.ok(true))
       allure.createStep('Ввести пароль', async() => {
-        await driver.findElement(By.id("loginform-password")).sendKeys("123456qQ_wrong")
-        assert.ok(true)
-      })
+        await driver.findElement(By.id("loginform-password")).sendKeys("123456qQ")
+      }, assert.ok(true))
       allure.createStep('Отправить форму', async() => {
         await driver.findElement(By.id("loginform-password")).sendKeys(Key.ENTER)
-        assert.ok(true)
       })
       allure.createStep('Авторизация успешна', async() => {
         await driver.wait(until.elementLocated(By.css(".g-header_profile_data_name")),30000)
         await driver.wait(until.elementIsVisible(driver.findElement(By.css(".g-header_profile_data_name"))))
-        assert.ok(true)
-      })
+      }, assert.ok(true))
   })
 
   it('Выбрать игру', async function() {
