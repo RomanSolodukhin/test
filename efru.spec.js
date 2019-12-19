de677ad980d7f6fb1948727a3558bd9456d4d22f
RomanSolodukhin <mauvaistonn@gmail.com>
2019-12-19 18:11:28 +0300

    f

--- efru.spec.js
+++ efru.spec.js
@@ -113,6 +113,17 @@ describe('Eternal Fury RU', function() {
       reportName: process.env.GIT_BRANCH+'/'+process.env.GIT_COMMIT+'/'+process.env.GIT_COMMITTER_NAME,
       reportUrl: process.env.GIT_URL
   };
+  jenkinsEnv2 = {
+    name1a: execName,
+    type2: "jenkins",
+    dfd: process.env.JENKINS_URL,
+    buildOrderff: process.env.BUILD_NUMBER,
+    buildNameff: process.env.JOB_NAME+' '+process.env.BUILD_DISPLAY_NAME,
+    buildUrl: process.env.BUILD_URL,
+    reportName1: process.env.GIT_BRANCH+'/'+process.env.GIT_COMMIT+'/'+process.env.GIT_COMMITTER_NAME,
+    reportUrl: process.env.GIT_URL
+};
+    allure.createExecutor(jenkinsEnv2)
     allure.createExecutor(jenkinsEnv)
   }
   catch(err) {
