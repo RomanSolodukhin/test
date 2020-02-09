ffdedc9ae6c556ddce66c5c705d92da78c626600
RomanSolodukhin <mauvaistonn@gmail.com>
2020-02-10 00:14:28 +0300

    f

--- creagames.com/page-object-en.js
+++ creagames.com/page-object-en.js
@@ -1,87 +1,82 @@
 const {By, Key, until} = require('selenium-webdriver');
 
-export default class Page {
+export class Page {
 	constructor() {
-		this._driver = driver;
-		this.url = 'https://creagames.com/';
-		this.title = 'CreaGames — browser MMORPGs';
-		this.facebookUrl = 'https://www.facebook.com/creagamesworld/';
-	}
-	open(this.url) {
-		this._driver.get(url);
-	}
-	maintenance() {
-		title: new TextString(By.css('body > div > h1'), 'Maintenance'),
-		apeal: new TextString(By.css('body > div > div.text-preview'), 'Dear friends,'),
-		text: new TextString(By.css('body > div > div.text'), 'The CreaGames portal is temporarily unavailable due to maintenance. When the website is back and better than ever, we will announce it in the Facebook groups of our projects.'),
-		conclusion: new TextString(By.css('body > div > div.text2'), 'Stay tuned!'),
-		socialText: new TextString(By.css('body > div > div.social_text'), 'Follow us on social media:'),
-		social() {
-			facebook: new Button(By.css('.fb'), function(selector) {
-				return this._driver.findElement(selector).getAttribute('href') == this.facebookUrl;
-			});
-		}
-	}
-	header() {
-		lang() {
-			list: new Button(By.css('.lang-list'), async function(selector) {
-				await this._driver.wait(until.elementIsVisible(this._driver.findElement(By.css('global-header-sub-menu'))));
-				return true;
-			}),
-			ru: new Button(By.css('.icon icon-ru'), function(selector) {
-				return this._driver.findElement(selector).getAttribute('href') == this.url+'/ru';
-			}),
-			en: new Button(By.css('.icon icon-en'), function(selector) {
-				return this._driver.findElement(selector).getAttribute('href') == this.url+'/en';
-			}),
-			fr: new Button(By.css('.icon icon-fr'), function(selector) {
-				return this._driver.findElement(selector).getAttribute('href') == this.url+'/fr';
-			}),
-			de: new Button(By.css('.icon icon-de'), function(selector) {
-				return this._driver.findElement(selector).getAttribute('href') == this.url+'/de';
-			}),
-			get current() {
-				return this._driver.findElement(By.css('html')).getAttribute('lang');
+		this._driver = driver,
+		this.url = 'https://creagames.com/',
+		this.title = 'CreaGames — browser MMORPGs',
+		this.facebookUrl = 'https://www.facebook.com/creagamesworld/',
+		this.maintenance: {
+				title: new TextString(By.css('body > div > h1'), 'Maintenance'),
+				apeal: new TextString(By.css('body > div > div.text-preview'), 'Dear friends,'),
+				text: new TextString(By.css('body > div > div.text'), 'The CreaGames portal is temporarily unavailable due to maintenance. When the website is back and better than ever, we will announce it in the Facebook groups of our projects.'),
+				conclusion: new TextString(By.css('body > div > div.text2'), 'Stay tuned!'),
+				socialText: new TextString(By.css('body > div > div.social_text'), 'Follow us on social media:'),
+				social: {
+					facebook: new Button(By.css('.fb'), LinkC.check(this.facebookUrl)
+				}
+		},
+		this.header: {
+			lang: {
+				list: new Button(By.css('.lang-list'), async function(selector) {
+					await this._driver.wait(until.elementIsVisible(this._driver.findElement(By.css('.global-header-sub-menu'))));
+					return true;
+				}),
+				ru: new Button(By.css('.icon icon-ru'), LinkC.check(selector, this.url+'/ru')),
+				en: new Button(By.css('.icon icon-en'), LinkC.check(selector, this.url+'/en')),
+				fr: new Button(By.css('.icon icon-fr'), LinkC.check(selector, this.url+'/fr')),
+				de: new Button(By.css('.icon icon-de'), LinkC.check(selector, this.url+'/de')),
+				current: function() {
+					return this._driver.findElement(By.css('html')).getAttribute('lang');
+				}
 			}
 		}
+	}
+	open(path) {
+		this._driver.get(path);
 	}
 	setLang(targetLang) {
-		let setLangPromise = new Promise(function(resolve, reject) {
-			if(targetLang == this.header.lang.current) reject(new Error('Выбранный язык уже установлен.'));
+			if(targetLang == this.header.lang.current) throw new Error('Выбранный язык уже установлен.');
 			async function() {
-				driver.actions().move({origin: driver.findElement(this.header.lang.list)}).perform();
-				await driver.wait(until.elementIsVisible(driver.findElement(this.header.lang[targetLang])));
-				driver.findElement(this.header.lang[targetLang]).click();
-				await driver.wait(this.header.lang.current == targetLang);
-				resolve(true);
-			}
-		})
-		setLangPromise.then(function(value) {
-			return true;
-		});
-		setLangPromise.catch(function(error) {
-			return error;
-	}
+				try {
+					driver.actions().move({origin: driver.findElement(this.header.lang.list)}).perform();
+					await driver.wait(until.elementIsVisible(driver.findElement(this.header.lang[targetLang])));
+					driver.findElement(this.header.lang[targetLang]).click();
+					await driver.wait(this.header.lang.current == targetLang);
+					return true;
+				}
+				catch(err) {
+					throw err;
+				}
+			};
+		}
+};
 
-}
-
-class TextString(selector, text) {
-	constructor() {
+class TextString {
+	constructor(selector, text) {
 		this.selector = selector;
+		this.text = text;
 	}
 	check() {
-		return Page._driver.findElement(this.selector).getText() == text;
+		return Page._driver.findElement(this.selector).getText() == this.text;
 	}
 }
 
-class Button(selector, result) {
-	constructor() {
+class Button {
+	constructor(selector, result) {
 		this.selector = selector;
+		this.result = result;
 	}
 	press() {
 		Page._driver.findElement(this.selector).click();
 	}
 	check() {
-		return result(this.selector);
+		return this.result(this.selector);
+	}
+}
+
+statis LinkC(selector, url) {
+	check() {
+		return Page._driver.findElement(selector).getAttribute('href') == url;
 	}
 }
